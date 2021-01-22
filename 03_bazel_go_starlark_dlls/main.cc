47d434cfebef34e34973c1a915da74aac94506af
josiahsrc <contact@josiahsaunders.com>
2021-01-22 16:49:17 -0700

    cleanup and setup java program

--- 03_bazel_go_starlark_dlls/main.cc
+++ 03_bazel_go_starlark_dlls/main.cc
@@ -1,9 +1,14 @@
 #include <iostream>
-#include "03_bazel_go_starlark_dlls/bridge/archive.h"
+#include "03_bazel_go_starlark_dlls/archive.h"
 
 int main()
 {
     std::cout << "Hello from the C code!" << std::endl;
-    // SayHello();
+
+    GoInt a = 12;
+    std::cout << "This is a GoInt: " << a << std::endl;
+
+    SayHello();
+
     return 0;
 }
