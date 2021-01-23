bdae421d966119e4699c8f28e2c22d3ad4b4d981
josiahsrc <contact@josiahsaunders.com>
2021-01-23 15:21:31 -0700

    more complicated java dlls with go

--- 04_bazel_java2go_starlark_dlls/hello.go
+++ 04_bazel_java2go_starlark_dlls/hello.go
@@ -3,15 +3,15 @@ package main
 import (
 	"fmt"
 	"math"
-	// "sort"
-	// "sync"
+	"sort"
+	"sync"
 	// star "go.starlark.net/starlark"
 )
 
 import "C"
 
-// var count int
-// var mtx sync.Mutex
+var count int
+var mtx sync.Mutex
 
 //export SayHello
 func SayHello() {
@@ -28,20 +28,18 @@ func Cosine(x float64) float64 {
 	return math.Cos(x)
 }
 
-// //export Sort
-// func Sort(vals []int) {
-// 	sort.Ints(vals)
-// }
+//export Sort
+func Sort(vals []int) {
+	sort.Ints(vals)
+}
 
-// //export Log
-// func Log(msg string) int {
-// 	mtx.Lock()
-// 	defer mtx.Unlock()
-
-// 	fmt.Println(msg)
-
-// 	count++
-// 	return count
-// }
+//export Log
+func Log(msg string) int {
+	mtx.Lock()
+	defer mtx.Unlock()
+	fmt.Println(msg)
+	count++
+	return count
+}
 
 func main() {}
--- 04_bazel_java2go_starlark_dlls/src/main/java/Main.java
+++ 04_bazel_java2go_starlark_dlls/src/main/java/Main.java
@@ -5,14 +5,42 @@ import java.io.*;
 import java.nio.file.*;
 import java.net.*;
 import java.util.*;
+import java.lang.reflect.*;
 
 public class Main {
     public interface Archive extends Library {
+        public class GoString extends Structure {
+            public static class ByValue extends GoString implements Structure.ByValue { }
+
+            public String p;
+            public long n;
+            
+            protected List getFieldOrder() {
+                return Arrays.asList(new String[]{ "p", "n" });
+            }
+        }
+
+        public class GoSlice extends Structure {
+            public static class ByValue extends GoSlice implements Structure.ByValue {}
+
+            public Pointer data;
+            public long len;
+            public long cap;
+
+            protected List getFieldOrder() {
+                return Arrays.asList(new String[]{"data", "len", "cap"});
+            }
+        }
+
         public void SayHello();
 
         public long Add(long a, long b);
 
         public double Cosine(double value);
+
+        public int Log(GoString.ByValue str);
+
+        public void Sort(GoSlice.ByValue vals);
     }
 
     public static void main(String[] args) throws Exception {
@@ -21,11 +49,60 @@ public class Main {
 
         File file = Native.extractFromResourcePath("/dynamic.so");
         System.out.println("EXTRACTED=" + file.toString()); 
-        Archive archive = (Archive) Native.loadLibrary(file.toString(), Archive.class);
+        Archive dll = (Archive) Native.load(file.toString(), Archive.class);
 
-        archive.SayHello();
+        // Print some info about the library
+        {
+            Method[] methods = dll.getClass().getDeclaredMethods();
+            System.out.println("\nAVAILABLE DLL METHODS:"); 
+            for (Method method : methods) {
+                System.out.println(method);
+            }
 
-        System.out.println("GO says 5+6=" + archive.Add(5, 6)); 
-        System.out.println("GO says Cos(5)=" + archive.Cosine(5)); 
+            System.out.println(""); 
+        }
+
+        dll.SayHello();
+
+        System.out.println("GO says 5+6=" + dll.Add(5, 6)); 
+        System.out.println("GO says Cos(5)=" + dll.Cosine(5)); 
+
+        System.out.println(""); 
+        System.out.println("Testing out Golang logging...");
+        {
+            Archive.GoString.ByValue stringToLog = new Archive.GoString.ByValue();
+            stringToLog.p = "Hello, message sent to Go from Java!";
+            stringToLog.n = stringToLog.p.length();
+
+            System.out.println("Logged with count=" + dll.Log(stringToLog));
+            System.out.println("Logged with count=" + dll.Log(stringToLog));
+            System.out.println("Logged with count=" + dll.Log(stringToLog));
+        }
+
+        System.out.println(""); 
+        System.out.println("Testing out Golang sorting...");
+        {
+            // First, prepare data array 
+            long[] nums = new long[]{53,11,5,2,88};
+            Memory arr = new Memory(nums.length * Native.getNativeSize(Long.TYPE));
+            arr.write(0, nums, 0, nums.length); 
+
+            // Fill in the GoSlice class for type mapping
+            Archive.GoSlice.ByValue slice = new Archive.GoSlice.ByValue();
+            slice.data = arr;
+            slice.len = nums.length;
+            slice.cap = nums.length;
+
+            dll.Sort(slice);
+
+            System.out.print("GO Sort(53,11,5,2,88) = [");
+            long[] sorted = slice.data.getLongArray(0,nums.length);
+
+            for(int i = 0; i < sorted.length; i++){
+                System.out.print(sorted[i] + " ");
+            }
+
+            System.out.println("]");
+        }
     }
 }
